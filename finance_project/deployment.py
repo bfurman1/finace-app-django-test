e4582927affc4b1a29b14ca0eba2eee314c3b156
Boris Furman <bfurman1@gmail.com>
2025-06-17 20:14:50 -0500

    import base path

--- finance_project/deployment.py
+++ finance_project/deployment.py
@@ -1,5 +1,6 @@
 import os
 from settings import *
+from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS= ['https://' + os.environ['WEBSITE_HOSTNAME']]
@@ -24,6 +25,9 @@ MIDDLEWARE = [
      "allauth.account.middleware.AccountMiddleware",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -33,7 +37,3 @@ DATABASES = {
         'PASSWORD': os.environ['AZURE_POSTGRESQL_PASS'],
     }
 }
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATIC_ROOT = os.path.join (BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / "staticfiles"
